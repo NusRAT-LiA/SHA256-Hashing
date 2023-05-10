00846addcb69a3a5f0074b58290b427ac92fc6be
NusRAT-Lia <nusratlia424@gmail.com>
2023-05-11 01:37:57 +0600

    All functions done

---
+++ src/Sha256.h
@@ -0,0 +1,10 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+#include "Sha256.cpp"
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+string Hash(string);
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Sha256.h"
+
+int main()
+{   
+    std::cout<<"Welcome to SHA-256 Hash Generator!\n"<<endl;
+
+    std::cout<<"Enter the message : ";
+    string inputString ;
+    std:: cin>>inputString;
+
+    std:: cout<<"SHA-256 hash : "<<Hash(inputString)<<endl;
+    std::cout<<"Hash computed successfully!"<<endl;
+}
